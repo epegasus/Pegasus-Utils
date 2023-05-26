355ee872219e8f74149e653c27911b61a93d9909
Sohaib <sohaibahmed.hypersoft@gmail.com>
2023-05-26 10:46:21 +0500

    Pegasus-Utils

---
+++ pegutils/src/main/java/dev/pegasus/utils/utils/PegasusCoroutineUtils.kt
@@ -0,0 +1,25 @@
+package dev.pegasus.utils.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.ContinuationInterceptor
+import kotlin.coroutines.coroutineContext
+
+/**
+ * @Author: SOHAIB AHMED
+ * @Date: 26,May,2023
+ * @Accounts
+ *      -> https://github.com/epegasus
+ *      -> https://stackoverflow.com/users/20440272/sohaib-ahmed
+ */
+
+object PegasusCoroutineUtils {
+
+    suspend fun checkCoroutineCurrentDispatcher(): String {
+        return when (coroutineContext[ContinuationInterceptor]) {
+            Dispatchers.Main -> "Dispatcher: Main"
+            Dispatchers.IO -> "Dispatcher: IO"
+            Dispatchers.Default -> "Dispatcher: Default"
+            else -> "Dispatcher: No Idea"
+        }
+    }
+}
