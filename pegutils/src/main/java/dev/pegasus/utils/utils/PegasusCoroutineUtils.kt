9b470a5cc81b15af93aa8a4b4459fa886cf891a9
Sohaib <sohaibahmed.hypersoft@gmail.com>
2023-05-26 10:46:21 +0500

    Pegasus-Utils

---
+++ pegutils/src/main/java/dev/pegasus/utils/utils/PegasusCoroutineUtils.kt
@@ -0,0 +1,11 @@
+package dev.pegasus.utils.utils
+
+/**
+ * @Author: SOHAIB AHMED
+ * @Date: 26,May,2023
+ * @Accounts
+ *      -> https://github.com/epegasus
+ *      -> https://stackoverflow.com/users/20440272/sohaib-ahmed
+ */
+ object PegasusCoroutineUtils {
+}
