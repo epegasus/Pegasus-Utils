0731cf0de88d542451182266f834817172405a0a
Sohaib Ahmed <sohaibmughal93@gmail.com>
2023-08-27 00:08:13 +0500

    PegUtils

--- pegutils/src/main/java/dev/pegasus/utils/utils/PegasusValidationUtils.kt
+++ pegutils/src/main/java/dev/pegasus/utils/utils/PegasusValidationUtils.kt
@@ -1,5 +1,6 @@
 package dev.pegasus.utils.utils
 
+import android.widget.AutoCompleteTextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -15,6 +16,19 @@ object PegasusValidationUtils {
 
     fun isNotEmpty(layout: TextInputLayout, edittext: TextInputEditText, errorMessage: String? = null): Boolean {
         val text = edittext.text?.trim().toString()
+        return if (text.isEmpty()) {
+            layout.error = errorMessage ?: "Field can't be empty."
+            layout.isErrorEnabled = true
+            false
+        } else {
+            layout.isErrorEnabled = false
+            layout.error = ""
+            true
+        }
+    }
+
+    fun isNotEmpty(layout: TextInputLayout, textView: AutoCompleteTextView, errorMessage: String? = null): Boolean {
+        val text = textView.text?.trim().toString()
         return if (text.isEmpty()) {
             layout.error = errorMessage ?: "Field can't be empty."
             layout.isErrorEnabled = true
