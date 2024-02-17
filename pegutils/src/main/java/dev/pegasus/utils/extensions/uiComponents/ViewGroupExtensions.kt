864e0dbff493ff591bf756ef13fe648d95070cbb
Sohaib Ahmed <sohaibmughal93@gmail.com>
2024-02-17 15:38:37 +0500

    PegUtils

--- pegutils/src/main/java/dev/pegasus/utils/extensions/uiComponents/ViewGroupExtensions.kt
+++ pegutils/src/main/java/dev/pegasus/utils/extensions/uiComponents/ViewGroupExtensions.kt
@@ -1,6 +1,7 @@
 package dev.pegasus.utils.extensions.uiComponents
 
 import android.content.res.Resources
+import android.os.Handler
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -62,4 +63,28 @@ fun ViewPager2.addCarouselEffect(enableZoom: Boolean = true) {
         }
     }
     setPageTransformer(compositePageTransformer)
+}
+
+/**
+ *  -> e.g. viewPager2.startAutoScroll(1000)
+ * @param  intervalMillis: Time in milliseconds to change the slide
+ * @param autoScrollHandler: Pass your own handler to handle scroll
+ *
+ * Note: passing same handler will be able to remove scroll
+ */
+
+fun ViewPager2.startAutoScroll(autoScrollHandler: Handler, intervalMillis: Long = 3000) {
+    val autoScrollRunnable = object : Runnable {
+        override fun run() {
+            val currentItem = currentItem
+            val nextItem = if (currentItem == adapter?.itemCount?.minus(1)) 0 else currentItem + 1
+            this@startAutoScroll.currentItem = nextItem
+            autoScrollHandler.postDelayed(this, intervalMillis)
+        }
+    }
+    autoScrollHandler.postDelayed(autoScrollRunnable, intervalMillis)
+}
+
+fun ViewPager2.stopAutoScroll(handler: Handler) {
+    handler.removeCallbacksAndMessages(null)
 }
